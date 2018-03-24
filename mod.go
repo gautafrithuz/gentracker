e2b8e70dfdc57e2555b14ba6f4a873761012f9cb
Jeffrey Lund <jefflund@gmail.com>
2018-03-24 09:48:56 -0600

    Read/Write works

--- mod.go
+++ mod.go
@@ -36,51 +36,48 @@ type Mod struct {
 	Patterns []Pattern
 }
 
-func Read(r io.Reader) (Mod, error) {
-	var mod Mod
-
+func (m *Mod) Read(r io.Reader) error {
 	name, err := readName(r, LEN_NAME)
 	if err != nil {
-		return Mod{}, err
+		return err
 	}
-	mod.Name = name
+	m.Name = name
 
-	// TODO read correct number of samples
+	// TODO read correct number of samples based on initials
 	for i := 0; i < NUM_SAMPLES; i++ {
-		sample, err := readSample(r)
+		sample, err := readSampleHead(r)
 		if err != nil {
-			return Mod{}, err
+			return err
 		}
-		mod.Samples = append(mod.Samples, sample)
+		m.Samples = append(m.Samples, sample)
 	}
 
 	len, err := readByte(r)
 	if err != nil {
-		return Mod{}, err
+		return err
 	}
 	// TODO check range [1,128]
-	mod.Len = len
+	m.Len = len
 
 	_, err = readByte(r)
 	if err != nil {
-		return Mod{}, err
+		return err
 	}
 	// TODO check =127
 
 	seq, err := readBytes(r, LEN_SEQUENCE)
 	if err != nil {
-		return Mod{}, err
+		return err
 	}
 	// TODO check values [0,63]
-	// TODO remove things past len?
-	mod.Sequence = seq
+	m.Sequence = seq
 
 	mk, err := readBytes(r, 4)
 	if err != nil {
-		return Mod{}, err
+		return err
 	}
 	if string(mk) != "M.K." {
-		return Mod{}, errors.New("Not M.K.")
+		return errors.New("Not M.K.")
 	}
 
 	var npatterns byte
@@ -92,25 +89,71 @@ func Read(r io.Reader) (Mod, error) {
 	for i := byte(0); i < npatterns; i++ {
 		pattern, err := readPattern(r)
 		if err != nil {
-			return Mod{}, nil
+			return err
 		}
-		mod.Patterns = append(mod.Patterns, pattern)
+		m.Patterns = append(m.Patterns, pattern)
 	}
 
 	for i := 0; i < NUM_SAMPLES; i++ {
-		if len := mod.Samples[i].Len; len > 0 {
+		if len := m.Samples[i].Len; len > 0 {
 			data, err := readBytes(r, int(len))
 			if err != nil {
-				return Mod{}, nil
+				return err
 			}
-			mod.Samples[i].Data = data
+			m.Samples[i].Data = data
 		}
 	}
 
-	return mod, nil
+	return nil
 }
 
-func readSample(r io.Reader) (Sample, error) {
+func (m *Mod) Write(w io.Writer) error {
+	if err := writeName(w, m.Name, LEN_NAME); err != nil {
+		return err
+	}
+
+	// TODO write correct number of samples based on initials
+	for i := 0; i < NUM_SAMPLES; i++ {
+		if err := writeSampleHead(w, m.Samples[i]); err != nil {
+			return err
+		}
+	}
+
+	// TODO check range [1,128]
+	if err := writeByte(w, m.Len); err != nil {
+		return err
+	}
+	// TODO check =127
+	if err := writeByte(w, 127); err != nil {
+		return err
+	}
+
+	// TODO check values [0,63]
+	if err := writeBytes(w, m.Sequence); err != nil {
+		return err
+	}
+
+	mk := []byte("M.K.")
+	if err := writeBytes(w, mk); err != nil {
+		return err
+	}
+
+	for _, p := range m.Patterns {
+		if err := writePattern(w, p); err != nil {
+			return err
+		}
+	}
+
+	for i := 0; i < NUM_SAMPLES; i++ {
+		if err := writeBytes(w, m.Samples[i].Data); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func readSampleHead(r io.Reader) (Sample, error) {
 	var sample Sample
 
 	name, err := readName(r, LEN_SAMPLE_NAME)
@@ -153,6 +196,28 @@ func readSample(r io.Reader) (Sample, error) {
 	return sample, nil
 }
 
+func writeSampleHead(w io.Writer, s Sample) error {
+	if err := writeName(w, s.Name, LEN_SAMPLE_NAME); err != nil {
+		return err
+	}
+	if err := writeWyde(w, s.Len/2); err != nil {
+		return err
+	}
+	if err := writeNibble(w, s.Tune); err != nil {
+		return err
+	}
+	if err := writeByte(w, s.Volume); err != nil {
+		return err
+	}
+	if err := writeWyde(w, s.RepeatStart/2); err != nil {
+		return err
+	}
+	if err := writeWyde(w, s.RepeatLen/2); err != nil {
+		return err
+	}
+	return nil
+}
+
 func readPattern(r io.Reader) (Pattern, error) {
 	data, err := readBytes(r, 1024)
 	if err != nil {
@@ -161,8 +226,12 @@ func readPattern(r io.Reader) (Pattern, error) {
 	return Pattern{data}, nil
 }
 
-func readName(r io.Reader, len int) ([]byte, error) {
-	name, err := readBytes(r, len)
+func writePattern(w io.Writer, p Pattern) error {
+	return writeBytes(w, p.Data)
+}
+
+func readName(r io.Reader, n int) ([]byte, error) {
+	name, err := readBytes(r, n)
 	if err != nil {
 		return nil, err
 	}
@@ -175,12 +244,25 @@ func readName(r io.Reader, len int) ([]byte, error) {
 	return name, nil
 }
 
+func writeName(w io.Writer, p []byte, n int) error {
+	for len(p) < n {
+		p = append(p, 0)
+	}
+	return writeBytes(w, p)
+}
+
 func readWyde(r io.Reader) (uint16, error) {
 	b, err := readBytes(r, 2)
 	if err != nil {
 		return 0, err
 	}
 	return binary.BigEndian.Uint16(b), nil
+}
+
+func writeWyde(w io.Writer, v uint16) error {
+	p := make([]byte, 2)
+	binary.BigEndian.PutUint16(p, v)
+	return writeBytes(w, p)
 }
 
 func readNibble(r io.Reader) (int8, error) {
@@ -191,12 +273,20 @@ func readNibble(r io.Reader) (int8, error) {
 	return int8(b[0] & 0x0F), nil
 }
 
+func writeNibble(w io.Writer, v int8) error {
+	return writeByte(w, byte(v))
+}
+
 func readByte(r io.Reader) (byte, error) {
 	b, err := readBytes(r, 1)
 	if err != nil {
 		return 0, err
 	}
 	return b[0], nil
+}
+
+func writeByte(w io.Writer, v byte) error {
+	return writeBytes(w, []byte{v})
 }
 
 func readBytes(r io.Reader, len int) ([]byte, error) {
@@ -206,4 +296,15 @@ func readBytes(r io.Reader, len int) ([]byte, error) {
 		return nil, err
 	}
 	return b, nil
+}
+
+func writeBytes(w io.Writer, p []byte) error {
+	n, err := w.Write(p)
+	if err != nil {
+		return err
+	}
+	if n != len(p) {
+		return errors.New("bad write")
+	}
+	return nil
 }
--- mod_test.go
+++ mod_test.go
@@ -1,65 +1,94 @@
 package amiga
 
 import (
+	"bytes"
+	"io"
 	"os"
 	"reflect"
 	"testing"
 )
 
 func TestRead(t *testing.T) {
+	var m Mod
+
 	f, err := os.Open("example.mod")
 	if err != nil {
 		panic(err)
 	}
-	mod, err := Read(f)
+	err = m.Read(f)
 	if err != nil {
 		t.Error(err)
 	}
 
-	if string(mod.Name) != "01-WARM" {
-		t.Errorf("Name: %s", mod.Name)
+	if string(m.Name) != "01-WARM" {
+		t.Errorf("Name: %s", m.Name)
 	}
 
-	if len(mod.Samples) != NUM_SAMPLES {
-		t.Errorf("Num Samples: %d", len(mod.Samples))
+	if len(m.Samples) != NUM_SAMPLES {
+		t.Errorf("Num Samples: %d", len(m.Samples))
 	}
-	if string(mod.Samples[8].Name) != "TECH-CRASH" {
-		t.Errorf("Sample Name: %s", mod.Samples[8].Name)
+	if string(m.Samples[8].Name) != "TECH-CRASH" {
+		t.Errorf("Sample Name: %s", m.Samples[8].Name)
 	}
-	if mod.Samples[8].Len != 15458 {
-		t.Errorf("Sample Len: %d", mod.Samples[8].Len)
+	if m.Samples[8].Len != 15458 {
+		t.Errorf("Sample Len: %d", m.Samples[8].Len)
 	}
-	if mod.Samples[8].Tune != 0 {
-		t.Errorf("Sample Tune: %d", mod.Samples[8].Tune)
+	if m.Samples[8].Tune != 0 {
+		t.Errorf("Sample Tune: %d", m.Samples[8].Tune)
 	}
-	if mod.Samples[8].Volume != 48 {
-		t.Errorf("Sample Volume: %d!=%d", mod.Samples[8].Volume)
+	if m.Samples[8].Volume != 48 {
+		t.Errorf("Sample Volume: %d!=%d", m.Samples[8].Volume)
 	}
-	if mod.Samples[8].RepeatStart != 13534 {
-		t.Errorf("Sample Repeat Start: %d", mod.Samples[8].RepeatStart)
+	if m.Samples[8].RepeatStart != 13534 {
+		t.Errorf("Sample Repeat Start: %d", m.Samples[8].RepeatStart)
 	}
-	if mod.Samples[8].RepeatLen != 1924 {
-		t.Errorf("Sample Repeat Start: %d", mod.Samples[8].RepeatLen)
+	if m.Samples[8].RepeatLen != 1924 {
+		t.Errorf("Sample Repeat Start: %d", m.Samples[8].RepeatLen)
 	}
 	lens := []uint16{52176, 25764, 27884, 40070, 24560, 30800, 256, 20722, 15458, 18724, 17640, 9014, 8820, 18724, 30646}
 	for len(lens) < NUM_SAMPLES {
 		lens = append(lens, 0)
 	}
 	for i := 0; i < NUM_SAMPLES; i++ {
-		if mod.Samples[i].Len != lens[i] {
-			t.Errorf("Sample Len %d: %d", i, mod.Samples[i].Len)
+		if m.Samples[i].Len != lens[i] {
+			t.Errorf("Sample Len %d: %d", i, m.Samples[i].Len)
 		}
 	}
 
-	if mod.Len != 48 {
-		t.Errorf("Len: %d", mod.Len)
+	if m.Len != 48 {
+		t.Errorf("Len: %d", m.Len)
 	}
 
 	seq := []byte{0x13, 0x03, 0x02, 0x03, 0x04, 0x05, 0x04, 0x06, 0x07, 0x08, 0x09, 0x08, 0x0C, 0x08, 0x0C, 0x08, 0x0A, 0x0B, 0x0A, 0x0B, 0x00, 0x01, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12, 0x11, 0x12, 0x00, 0x01, 0x16, 0x14, 0x15, 0x17, 0x18, 0x17, 0x19, 0x19, 0x19, 0x19, 0x1C, 0x1D, 0x1E, 0x1D, 0x1B, 0x1A}
 	for len(seq) < LEN_SEQUENCE {
 		seq = append(seq, 0)
 	}
-	if !reflect.DeepEqual(mod.Sequence, seq) {
+	if !reflect.DeepEqual(m.Sequence, seq) {
 		t.Errorf("Mod Seq")
+	}
+}
+
+func TestWrite(t *testing.T) {
+	var m Mod
+
+	f, err := os.Open("example.mod")
+	if err != nil {
+		panic(err)
+	}
+	var expected bytes.Buffer
+	tee := io.TeeReader(f, &expected)
+	err = m.Read(tee)
+	if err != nil {
+		t.Error(err)
+	}
+
+	var actual bytes.Buffer
+	err = m.Write(&actual)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !reflect.DeepEqual(actual.Bytes(), expected.Bytes()) {
+		t.Error("Write not binary equal!")
 	}
 }
